35eea2a5400afa73b007116be6f39b0d7b4bcf66
ChristopherallanB <120734179+christopherallanb@users.noreply.github.com>
2024-10-16 16:13:34 -0300

    Add files via upload

---
+++ calcula os lados da hipotenuza.py
@@ -0,0 +1,28 @@
+import math
+
+cateto_1 = float(input("Por favor, digite o comprimento do primeiro cateto (em metros): "))
+cateto_2 = float(input("digite o comprimento do segundo cateto (em metros): "))
+
+hipotenusa = math.sqrt(cateto_1 ** 2 + cateto_2 ** 2)
+
+print(f"\n  O comprimento da hipotenusa é aproximadamente: {hipotenusa:.2f} metros.")
+
+
+                    
+                                      ##          
+                                      ############
+                                    ##############
+                                  ##############  
+                            ##################    
+                      ####################        
+      ##            ####################          
+                  ######################          
+    ##            ######################          
+    ##            ######################          
+  ##            ##################      ##        
+  ##          ##################        ##        
+  ##          ################                    
+  ##        ##################                    
+  ####    ####################                    
+    ################################              
+                                                  
